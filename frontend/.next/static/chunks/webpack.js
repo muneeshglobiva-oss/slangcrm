b831c33ff778dd6ccf165571731e7e175e2b599c
Muneeshxxx <max@mecanmarine.com>
2025-12-25 11:01:35 +0530

    latest

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7e60938460688b0"; }
+/******/ 		__webpack_require__.h = function() { return "9ac90c8598d848bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
